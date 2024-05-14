c997f99d8ce864d9926d5d59ce4dc4f909fb580a
Ludovic Lahougue <ludorjt12@gmail.com>
2024-05-14 11:43:22 +0200

    fix

---
+++ front/src/atoms/contactAtoms.js
@@ -0,0 +1,7 @@
+import { atom } from 'jotai';
+
+export const socialNetworksAtom = atom(undefined);
+
+export const allSocialNetworksAtom = atom(undefined);
+
+export const selectedSocialNetworkAtom = atom(undefined);
