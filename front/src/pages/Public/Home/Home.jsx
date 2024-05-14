8ff6977a6f9479cf0b014c02a5dcc0eb9bc002e0
Corentin_Pageot <pageot.corentin@orange.fr>
2024-05-14 09:50:53 +0200

    visual changes for the home page, menu and footer

--- front/src/pages/Public/Home/Home.jsx
+++ front/src/pages/Public/Home/Home.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='flex-grow-1'></div>
+        <div  className='relative-container'>
+            <video autoPlay loop muted className='w-full video-height'>
+                <source src="https://video.wixstatic.com/video/11062b_b07b4c36aacc42aa86ce4bd9b8d3afd3/720p/mp4/file.mp4" type="video/mp4" />
+            </video>
+            <div className='overlay text-center'>
+                <h1 className='mb-5 font-light'>Notre élevage est situé dans le Calvados, en Normandie</h1>
+                <a href='/contact' className='contact-link no-underline'>Nous contacter pour plus d'informations</a>
+            </div>
+        </div>
     )
 }
 
